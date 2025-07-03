eef250faa1e7ec7738a8a2f4ab497e547c3cee54
Tanmay Mishu <tanmaymishu@gmail.com>
2025-07-03 20:11:23 +0600

    Redirect to /articles if the user has already verified email

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -4,9 +4,16 @@ import React, {useState} from "react";
 import {Button} from "@/components/ui/button";
 import axios, {isAxiosError} from "@/lib/axios";
 import {toast} from "sonner";
+import useAuth from "@/hooks/use-auth";
+import {redirect} from "next/navigation";
 
 function Forbidden() {
   const [loading, setLoading] = useState(false);
+  const {user} = useAuth();
+
+  if (user?.email_verified_at) {
+    redirect('/articles');
+  }
 
   async function sendVerificationLink() {
     try {
@@ -29,7 +36,8 @@ function Forbidden() {
     <div className="flex h-screen">
       <section className="m-auto text-2xl flex items-center flex-col gap-4">
         <p className="text-xl text-center">Please verify your e-mail address to access this page.</p>
-        <p className="text-xs text-center">We&#39;ve sent you a verification link to your e-mail address. Click on the link to verify and continue accessing the private pages.</p>
+        <p className="text-xs text-center">We&#39;ve sent you a verification link to your e-mail address. Click on the
+          link to verify and continue accessing the private pages.</p>
         <Button className="w-fit" onClick={sendVerificationLink} disabled={loading}>Resend Verification Link</Button>
       </section>
     </div>
